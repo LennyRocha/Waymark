6c14215512a8bc2a83b705902cb734f963093619
LennyRocha <20233tn094@utez.edu.mx>
2026-03-07 14:42:27 -0600

    [ADD] Bases del front (instalacion de dependencias, configuración de router y estructuración de carpetas) creadas.

---
+++ frontend/src/modules/propiedades/RutaRandom.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export default function RutaRandom() {
+  return (
+    <div>
+        <p>RutaRandom es la subruta</p>
+        <Link to="/">Home</Link>
+    </div>
+  )
+}
---
+++ frontend/src/utils/Icono.jsx
@@ -0,0 +1,7 @@
+import { DynamicIcon } from 'lucide-react/dynamic';
+
+export default function Icono ({ props }) {
+  return <DynamicIcon {...props} />
+};
+
+//Documentación de lucide icons: https://lucide.dev/guide/packages/lucide-react
