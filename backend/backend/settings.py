62bc8acf2faacb3fc41535c7b00f1d8f4904baab
LennyRocha <20233tn094@utez.edu.mx>
2026-04-16 09:47:38 -0600

    [ADD] Iniciador de tablas agregado.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "initialdata.apps.InitialdataConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
