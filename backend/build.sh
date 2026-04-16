6e869aef6ad89b639b7ae02259e6992bb47e922c
LennyRocha <20233tn094@utez.edu.mx>
2026-04-16 11:55:52 -0600

    [FIX] otro fix.

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-set -e
-
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
-
 python manage.py migrate
 
 python manage.py loaddata initialdata/fixtures/amenidades.json
