03a73b291c20dcd38dd1f08e54fc107d27e37cb7
LennyRocha <20233tn094@utez.edu.mx>
2026-04-15 22:06:45 -0600

    [FEAT] Placeholder de ip del front.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -139,7 +139,11 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Puerto por defecto de Vite
+    "http://127.0.0.1:5173",
+    "http://192.168.0.0:5173",
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
