4f51453acea1058261b9b48e17f7e72a30e45de4
LennyRocha <20233tn094@utez.edu.mx>
2026-04-16 11:53:30 -0600

    [UPDATE] Build.sh.

--- backend/build.sh
+++ backend/build.sh
@@ -3,3 +3,15 @@ set -e
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+python manage.py migrate
+
+python manage.py loaddata initialdata/fixtures/amenidades.json
+python manage.py loaddata initialdata/fixtures/divisas.json
+python manage.py loaddata initialdata/fixtures/doc_estado.json
+python manage.py loaddata initialdata/fixtures/doc_tipo.json
+python manage.py loaddata initialdata/fixtures/reserva_estado.json
+python manage.py loaddata initialdata/fixtures/roles.json
+python manage.py loaddata initialdata/fixtures/tipo_propiedades.json    
+
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000
