0cfebaab96d99897b635e249250ecfe6dfa33fa6
LennyRocha <20233tn094@utez.edu.mx>
2026-04-20 12:30:58 -0600

    [FIX] Interceptor de token en landing.

--- frontend/src/utils/apiLanding.ts
+++ frontend/src/utils/apiLanding.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import api from "./api";
 
 const url = import.meta.env.VITE_API_URL;
 
@@ -14,5 +15,52 @@ apiLanding.interceptors.request.use((config) => {
     }
     return config;
 });
+
+apiLanding.interceptors.response.use(
+    (response) => response,
+
+    async (error) => {
+    const originalRequest = error.config;
+
+    if (
+        error.response?.status === 401 &&
+        !originalRequest._retry
+    ) {
+        originalRequest._retry = true;
+
+        const refreshToken =
+            localStorage.getItem("refresh_token");
+
+        if (!refreshToken) {
+            delete originalRequest.headers.Authorization;
+            return api(originalRequest);
+        }
+
+        try {
+        const response = await axios.post(
+            `${url}/token/refresh/`,
+            {
+                refresh: refreshToken,
+            },
+        );
+
+        const newAccess = response.data.access;
+
+        // Guardar nuevo access
+        localStorage.setItem("access_token", newAccess);
+
+        // Reintentar request original
+        originalRequest.headers.Authorization = `Bearer ${newAccess}`;
+
+        return apiLanding(originalRequest);
+        } catch {
+        delete originalRequest.headers.Authorization;
+        return api(originalRequest);
+        }
+    }
+
+    throw error;
+    },
+);
 
 export default apiLanding;
